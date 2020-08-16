1c207774ad4adc02433e4fd0b382d74d5b564a7e
medram <mramouchy@gmail.com>
2020-08-16 16:53:59 +0100

    fixed insight permissions

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+
+app_name = __package__
+
+urlpatterns = [
+	# path('sokna/', views.create_sokna, name='create_sokna'),
+	# path('sokna/<int:id>/', views.sokna_detail, name='sokna_detail')
+	path('sokna/', views.SoknaList.as_view()),
+	path('sokna/<int:pk>/', views.SoknaDetail.as_view()),
+	path('administrative-attache/', views.Mol7akaList.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
