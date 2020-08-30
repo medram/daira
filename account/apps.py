a8a7d17b6aef7183ebe495467428d3490a7906b7
medram <mramouchy@gmail.com>
2020-08-30 15:03:07 +0100

    fixed some Adminsite design for Django 3.1

--- account/apps.py
+++ account/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class AccountConfig(AppConfig):
     name = 'account'
-    verbose_name = 'Accounts Manager (AUTHENTICATION AND AUTHORIZATION)'
+    verbose_name = 'Accounts Managements'
 
     def ready(self):
     	import account.signals
