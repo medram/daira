f8466f6384e6248c20976b742eea7c48a8de2822
medram <mramouchy@gmail.com>
2020-09-14 00:01:32 +0100

    fixed some 'pages' module bugs

--- pages/admin.py
+++ pages/admin.py
@@ -3,36 +3,34 @@ from .models import Menu, MenuItem, Page
 
 from adminsortable2.admin import SortableInlineAdminMixin
 
+
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-	list_per_page = 25
-	list_display = ('title', 'slug', 'published', 'created', 'updated')
-	list_editable = ('published',)
-	list_filter = ('published',)
-	search_fields = ('title', 'slug')
+    list_per_page = 25
+    list_display = ('title', 'slug', 'published', 'created', 'updated')
+    list_editable = ('published',)
+    list_filter = ('published',)
+    search_fields = ('title', 'slug')
 
 
 class InLineMenuItem(SortableInlineAdminMixin, admin.TabularInline):
-	model = MenuItem
-	extra = 0
-	# max_num = 7
+    model = MenuItem
+    extra = 0
+    # max_num = 7
 
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-	inlines = (InLineMenuItem,)
-	list_display = ('name', 'total_menu_items')
-	readonly_fields = ('name',)
+    inlines = (InLineMenuItem,)
+    list_display = ('name', 'total_menu_items')
+    ordering = ('pk',)
+    readonly_fields = ('name',)
 
-	def total_menu_items(self, obj):
-		return MenuItem.objects.filter(menu=obj.pk).count()
+    def total_menu_items(self, obj):
+        return MenuItem.objects.filter(menu=obj.pk).count()
 
-	def has_delete_permission(self, request, obj=None):
-		return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
-	def has_add_permission(self, request, obj=None):
-		return False
-
-# admin.site.site_header = 'site header'
-# admin.site.site_title = 'site title'
-# admin.site.index_title = 'index title'
+    def has_add_permission(self, request, obj=None):
+        return False
--- pages/templatetags/pages_tags.py
+++ pages/templatetags/pages_tags.py
@@ -4,17 +4,26 @@ from ..models import Menu, MenuItem
 
 register = template.Library()
 
+
 @register.inclusion_tag('pages/tags/_menu_items.html', takes_context=True)
 def main_menu(context):
-	return {
-		'items': MenuItem.objects.filter(menu=2).order_by('order'),
-		'request': context.request
-	}
+    return {
+        'items': MenuItem.objects.filter(menu=1).order_by('order'),
+        'request': context.request
+    }
+
+
+@register.inclusion_tag('pages/tags/_menu_items.html', takes_context=True)
+def footer_menu(context):
+    return {
+        'items': MenuItem.objects.filter(menu=2).order_by('order'),
+        'request': context.request
+    }
 
 
 @register.inclusion_tag('pages/tags/_render_menu_item.html', takes_context=True)
 def render_menu_item(context, item):
-	return {
-		'item': item,
-		'request': context.request
-	}
+    return {
+        'item': item,
+        'request': context.request
+    }
