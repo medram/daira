7968de632dbf69223598e920b627e59d8df403df
medram <mramouchy@gmail.com>
2020-09-14 23:46:17 +0100

    fixed: follow submission, the id should be integer

--- sokna/views.py
+++ sokna/views.py
@@ -6,49 +6,50 @@ from .models import SoknaRequest
 
 def register(req):
 
-	if req.method == 'POST':
-		form = SoknaRequestForm(req.POST, req.FILES)
-		if form.is_valid():
-			instance = form.save()
-			messages.success(req, '<i class="fa fa-fw fa-check"></i> %s' % _('Your request has been submitted successfully.'))
-			# redirect to a valid page.
-			return redirect('sokna:done', id=instance.pk)
-	else:
-		form = SoknaRequestForm()
+    if req.method == 'POST':
+        form = SoknaRequestForm(req.POST, req.FILES)
+        if form.is_valid():
+            instance = form.save()
+            messages.success(req, '<i class="fa fa-fw fa-check"></i> %s' %
+                             _('Your request has been submitted successfully.'))
+            # redirect to a valid page.
+            return redirect('sokna:done', id=instance.pk)
+    else:
+        form = SoknaRequestForm()
 
-
-	page = {
-		'title': 'sokna'
-	}
-	return render(req, 'sokna/register.html', {'page': page, 'form': form})
+    page = {
+        'title': 'sokna'
+    }
+    return render(req, 'sokna/register.html', {'page': page, 'form': form})
 
 
 def done(req, id=None):
 
-	return render(req, 'sokna/done.html', {'code': id})
+    return render(req, 'sokna/done.html', {'code': id})
 
 
 def follow(req):
-	SR = None
-	if req.GET.get('CIN', None):
-		form = FollowForm(req.GET)
-		if form.is_valid():
-			# TODO: get SoknaRequest and show its status & notes
-			try:
-				SR = SoknaRequest.objects.get(id=form.cleaned_data['submission_code'], CIN=form.cleaned_data['CIN'])
-			except SoknaRequest.DoesNotExist:
-				messages.warning(req, 'Oops! There is no submission')
-				return redirect('sokna:follow')
-	else:
-		form = FollowForm()
+    SR = None
+    if req.GET.get('CIN', None):
+        form = FollowForm(req.GET)
+        if form.is_valid():
+            # TODO: get SoknaRequest and show its status & notes
+            try:
+                SR = SoknaRequest.objects.get(
+                    id=form.cleaned_data['submission_code'], CIN=form.cleaned_data['CIN'])
+            except (SoknaRequest.DoesNotExist, ValueError):
+                messages.warning(req, 'Oops! There is no submission')
+                return redirect('sokna:follow')
+    else:
+        form = FollowForm()
 
-	page = {
-		'title': 'Follow submission'
-	}
+    page = {
+        'title': 'Follow submission'
+    }
 
-	return render(req, 'sokna/follow.html', {'page': page, 'form': form, 'sokna_request': SR})
+    return render(req, 'sokna/follow.html', {'page': page, 'form': form, 'sokna_request': SR})
 
 
 def index(req):
 
-	return render(req, 'sokna/index.html')
+    return render(req, 'sokna/index.html')
